f7d5d03fa2991f55ac31271d01b7bba677d0c21c
MajaMarkiewicz <markiewicz.maja@gmail.com>
2019-07-27 19:17:06 +0200

    Updates

---
+++ precache-manifest.082165810d1321445c16096299fd9f8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9432e4b711b706f3a7037789a191b99c",
+    "url": "/React-TodoList/index.html"
+  },
+  {
+    "revision": "8cf98e3364b6d5b522f8",
+    "url": "/React-TodoList/static/css/main.2ea36b66.chunk.css"
+  },
+  {
+    "revision": "b0f8f685a7c90b1bb1c1",
+    "url": "/React-TodoList/static/js/2.b0950fc1.chunk.js"
+  },
+  {
+    "revision": "8cf98e3364b6d5b522f8",
+    "url": "/React-TodoList/static/js/main.23744914.chunk.js"
+  },
+  {
+    "revision": "108dbe416174f98bd927",
+    "url": "/React-TodoList/static/js/runtime~main.9330353c.js"
+  }
+]);
